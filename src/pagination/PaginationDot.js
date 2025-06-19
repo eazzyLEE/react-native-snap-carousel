a4cf6e5c8f366d9c2dd60c86e0c1617d48641707
Israel Ohue <israelohue@gmail.com>
2025-06-19 13:19:13 +0100

    Update PaginationDot.js

--- src/pagination/PaginationDot.js
+++ src/pagination/PaginationDot.js
@@ -12,11 +12,11 @@ export default class PaginationDot extends PureComponent {
         activeOpacity: PropTypes.number,
         carouselRef: PropTypes.object,
         color: PropTypes.string,
-        containerStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
+        containerStyle: ViewPropTypes ? ViewPropTypes.style : PropTypes.any,
         inactiveColor: PropTypes.string,
-        inactiveStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
+        inactiveStyle: ViewPropTypes ? ViewPropTypes.style : PropTypes.any,
         index: PropTypes.number,
-        style: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
+        style: ViewPropTypes ? ViewPropTypes.style : PropTypes.any,
         tappable: PropTypes.bool
     };
 
